f577dc7d2f2f06401f9fc6dbf9051a9ca37770a2
WorstProgrammer1 <suzuki.takeo.haltokyo@gmail.com>
2024-03-01 12:03:32 +0900

    Enemy

--- Assets/State_Enemy_Idle.cs
+++ Assets/State_Enemy_Idle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class State_Enemy_Idle : IState
+public class State_Enemy_Idle : MonoBehaviour
 {
     private Control_Enemy enemy;
 
