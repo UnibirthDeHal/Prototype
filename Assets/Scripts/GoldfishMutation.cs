fd4ad66bc31131f6bdc6aef8bd76c23ffc540953
WataEban <WatanabeTomoki3131@gmail.com>
2024-03-19 17:42:36 +0900

    金魚の変異状態管理スクリプト(途中まで)

---
+++ Assets/Scripts/GoldfishMutation.cs
@@ -0,0 +1,90 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MutationState
+{
+    Bad,
+    Good,
+    Great,
+    Caught,
+    Break
+}
+
+public class MutationStateMachine : MonoBehaviour
+{
+    private MutationState currentState = MutationState.Bad;
+
+    public void ApplyMutation(MutationState newState)
+    {
+        currentState = newState;
+        UpdateMutationState();
+    }
+
+    private void UpdateMutationState()
+    {
+        switch (currentState)
+        {
+            case MutationState.Bad:
+                
+                break;
+            case MutationState.Good:
+                
+                break;
+            case MutationState.Great:
+                
+                break;
+            case MutationState.Caught:
+                
+                break;
+            case MutationState.Break:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetGoodMutation()
+    {
+        ApplyMutation(MutationState.Good);
+    }
+
+    public void SetGreatMutation()
+    {
+        ApplyMutation(MutationState.Great);
+    }
+    public void SetCaughtMutation()
+    {
+        ApplyMutation(MutationState.Caught);
+    }
+    public void SetBreakMutation()
+    {
+        ApplyMutation(MutationState.Break);
+    }
+}
+
+public class GoldfishMutation : MonoBehaviour
+{
+        public MutationStateMachine mutationStateMachine;
+
+    public void ApplyGoodMutation()
+    {
+        mutationStateMachine.SetGoodMutation();
+    }
+
+    public void ApplyGreatMutation()
+    {
+        mutationStateMachine.SetGreatMutation();
+    }
+    public void ApplyCaughtMutation()
+    {
+        mutationStateMachine.SetCaughtMutation();
+    }
+
+    public void ApplyBreakMutation()
+    {
+        mutationStateMachine.SetBreakMutation();
+    }
+}
